78a58ee14fc3c9243d0c296a9f4d2b24555a335b
janhav-i2006 <janhvithorve7@gmail.com>
2026-06-04 16:43:20 +0530

    update resume

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-document.querySelectorAll('.card').forEach(card=>{
-card.addEventListener('mouseenter',()=>{
-card.style.boxShadow='0 0 20px #f59e0b';
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.style.boxShadow = '0 0 20px #f59e0b';
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.boxShadow = 'none';
+    });
 });
 
-card.addEventListener('mouseleave',()=>{
-card.style.boxShadow='none';
-});
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    document.getElementById("successCard").style.display = "block";
+
+    this.reset();
+
 });
